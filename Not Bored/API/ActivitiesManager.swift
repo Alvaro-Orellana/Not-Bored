327ef5e5bee2f66274e63d111e619aa884409b62
Alvaro-Orellana <alvaro.orellana@gmail.com>
2021-11-04 18:39:06 -0300

    All screens added and network calls working

--- Not Bored/API/ActivitiesManager.swift
+++ Not Bored/API/ActivitiesManager.swift
@@ -4,39 +4,88 @@
 //
 //  Created by Alvaro Hernan Orellana Villarroel on 03/11/2021.
 //
-
 import Foundation
 import Alamofire
 
-struct ActivitiesManager {
+class ActivitiesManager {
+    
     static let shared = ActivitiesManager()
     private init() {}
     
-    func getActivities(completion: @escaping ([Activity]) -> Void, parameters : [String:Any] = [:]) {
-        let url : String = "https://www.boredapi.com/api/activity"
-        //Si el array de parameters NO está vacio, debe completar el URL base
-        //con los parametros correspondientes
-        if !parameters.isEmpty{
+    let baseURL : String = "https://www.boredapi.com/api/activity"
+    var randomActivity: Activity?
+
+    
+    func getActivity(type: String, completion: @escaping (Activity?) -> Void) {
+        let url = "\(baseURL)?type=\(type)"
+        
+        AF.request(url).response { response in
+            switch response.result {
+            case .success(let data) :
+                do{
+                    if let data = data {
+                        let activity = try JSONDecoder().decode(Activity.self, from: data)
+                        completion(activity)
+                    }
+                } catch{
+                    print(error.localizedDescription)
+                    completion(nil)
+                }
+            case .failure(let error) :
+                print(error.localizedDescription)
+                completion(nil)
+            }
         }
         
-        let request = AF.request(url)
+    }
+    
+    
+    
+    func getRandomActivity(completion: @escaping (Activity?) -> Void) {
+//        AF.request(baseURL).response { [weak self] response in
+//            switch response.result {
+//
+//            case .success(let data):
+//                if let safeData = data {
+//                    self?.randomActivity = try? JSONDecoder().decode(Activity.self, from: safeData)
+//                }
+//
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            }
+//        }
+//
+//        return randomActivity
+//
+      
+        
+        let request = AF.request(baseURL)
         request.response { response in
             switch response.result {
             case .success(let data) :
                 do{
                     if let data = data {
                         let activity = try JSONDecoder().decode(Activity.self, from: data)
-                        completion([activity])
+                        completion(activity)
                     }
                 } catch{
                     print(error.localizedDescription)
-                    completion([])
+                    completion(nil)
                 }
             case .failure(let error) :
                 print(error.localizedDescription)
-                completion([])
+                completion(nil)
             }
         }
+        
+       
+        
+        
+        
+        
+        
+        
     }
     
 }
+
